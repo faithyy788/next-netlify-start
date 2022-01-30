aa6b4a6ce3a040fd9099494d6fa2e26d03fc2fe2
faithyy788 <83411907+faithyy788@users.noreply.github.com>
2022-01-30 22:38:15 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,8 +13,21 @@ export default function Home() {
       <main>
         <Header title="Welcome to my world!" />
         <p className="description">
-          I'm a Web developer<code>pages/A writer </code>
+          I'm a Web developer<code>A Computer Science graduate from Bowen university</code>
         </p>
+<h2>Web Designer and Developer</h2>
+<p><strong>Services</strong>
+<h3>Web Design </h3>
+<h3>Web Development</h3>
+<h3>Project Management </h3>
+<h3>Writer</h3>
+</p>
+
+<h3>My portfolio: </h3>
+
+<h3>Stay in touch </h3>
+<a href=""
+
       </main>
 
       <Footer />
